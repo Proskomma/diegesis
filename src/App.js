0fcca86b8ed3cac95ade7796cde4fa8d9478da24
Mark Howe <mvahowe@gmail.com>
2021-10-23 14:27:22 +0200

    favicon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useContext } from 'react';
+import React, {useContext} from 'react';
 import {Redirect, Route} from 'react-router-dom';
 import {IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs,} from '@ionic/react';
 import {IonReactRouter} from '@ionic/react-router';
-import { book, create, print, construct, cog } from 'ionicons/icons';
+import {book, cog, construct, create, print} from 'ionicons/icons';
 import BrowseTab from './pages/BrowseTab';
 import EditTab from './pages/EditTab';
 import PublishTab from './pages/PublishTab';
@@ -29,57 +29,56 @@ import '@ionic/react/css/display.css';
 import './theme/variables.css';
 
 /* Non-ionic imports */
-import PkContext from './PkContext'
-import { PkProvider } from './PkContext'
+import PkContext, {PkProvider} from './PkContext'
 
 const App = () => {
     const pk = useContext(PkContext);
     return <IonApp>
         <PkProvider value={pk}>
-        <IonReactRouter>
-            <IonTabs>
-                <IonRouterOutlet>
-                    <Route exact path="/browse">
-                        <BrowseTab/>
-                    </Route>
-                    <Route exact path="/edit">
-                        <EditTab/>
-                    </Route>
-                    <Route exact path="/publish">
-                        <PublishTab/>
-                    </Route>
-                    <Route exact path="/settings">
-                        <SettingsTab/>
-                    </Route>
-                    <Route exact path="/contribute">
-                        <ContributeTab/>
-                    </Route>
-                    <Route render={() => <Redirect to="/browse"/>} />
-                </IonRouterOutlet>
-                <IonTabBar slot="bottom">
-                    <IonTabButton tab="browse" href="/browse">
-                        <IonIcon icon={book}/>
-                        <IonLabel>Browse</IonLabel>
-                    </IonTabButton>
-                    <IonTabButton tab="edit" href="/edit">
-                        <IonIcon icon={create}/>
-                        <IonLabel>Edit</IonLabel>
-                    </IonTabButton>
-                    <IonTabButton tab="publish" href="/publish">
-                        <IonIcon icon={print}/>
-                        <IonLabel>Publish</IonLabel>
-                    </IonTabButton>
-                    <IonTabButton tab="contribute" href="/contribute">
-                        <IonIcon icon={construct}/>
-                        <IonLabel>Contribute</IonLabel>
-                    </IonTabButton>
-                    <IonTabButton tab="settings" href="/settings">
-                        <IonIcon icon={cog}/>
-                        <IonLabel>Settings</IonLabel>
-                    </IonTabButton>
-                </IonTabBar>
-            </IonTabs>
-        </IonReactRouter>
+            <IonReactRouter>
+                <IonTabs>
+                    <IonRouterOutlet>
+                        <Route exact path="/browse">
+                            <BrowseTab/>
+                        </Route>
+                        <Route exact path="/edit">
+                            <EditTab/>
+                        </Route>
+                        <Route exact path="/publish">
+                            <PublishTab/>
+                        </Route>
+                        <Route exact path="/settings">
+                            <SettingsTab/>
+                        </Route>
+                        <Route exact path="/contribute">
+                            <ContributeTab/>
+                        </Route>
+                        <Route render={() => <Redirect to="/browse"/>}/>
+                    </IonRouterOutlet>
+                    <IonTabBar slot="bottom">
+                        <IonTabButton tab="browse" href="/browse">
+                            <IonIcon icon={book}/>
+                            <IonLabel>Browse</IonLabel>
+                        </IonTabButton>
+                        <IonTabButton tab="edit" href="/edit">
+                            <IonIcon icon={create}/>
+                            <IonLabel>Edit</IonLabel>
+                        </IonTabButton>
+                        <IonTabButton tab="publish" href="/publish">
+                            <IonIcon icon={print}/>
+                            <IonLabel>Publish</IonLabel>
+                        </IonTabButton>
+                        <IonTabButton tab="contribute" href="/contribute">
+                            <IonIcon icon={construct}/>
+                            <IonLabel>Contribute</IonLabel>
+                        </IonTabButton>
+                        <IonTabButton tab="settings" href="/settings">
+                            <IonIcon icon={cog}/>
+                            <IonLabel>Settings</IonLabel>
+                        </IonTabButton>
+                    </IonTabBar>
+                </IonTabs>
+            </IonReactRouter>
         </PkProvider>
     </IonApp>
 };

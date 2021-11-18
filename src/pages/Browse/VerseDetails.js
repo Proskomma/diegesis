de7afa5d1424d7c68d215b9fe59ae76a82b24533
Mark Howe <mvahowe@gmail.com>
2021-11-18 14:35:44 +0100

    More compact, scaling syntax tree display

--- src/pages/Browse/VerseDetails.js
+++ src/pages/Browse/VerseDetails.js
@@ -1,7 +1,12 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import ReactMarkdown from 'react-markdown';
+import Tree from 'react-d3-tree';
 import {IonButton, IonCol, IonRow, IonText, IonTitle} from '@ionic/react';
+import './BrowseTab.css';
+import './TreeSVG.css';
+import useWindowDimensions from '../../components/useWindowDimensions';
 import PkContext from "../../contexts/PkContext";
+
 const xre = require('xregexp');
 
 const renderSyntaxTree = tribos => {
@@ -9,11 +14,56 @@ const renderSyntaxTree = tribos => {
         tree =>
             tree.children && tree.children.length === 1 ?
                 renderSyntaxTree([tree.children[0]]) :
-        <li>
-            {tree.content.elementType === 'w' ? `${tree.content.text} - ${tree.content.gloss} (${tree.content.class})`: tree.content.class || JSON.stringify(tree.content)}
-            {tree.children && <ul>{renderSyntaxTree(tree.children)}</ul>}
-        </li>
+                <li>
+                    {tree.content.elementType === 'w' ? `${tree.content.text} - ${tree.content.gloss} (${tree.content.class})` : tree.content.class || JSON.stringify(tree.content)}
+                    {tree.children && <ul>{renderSyntaxTree(tree.children)}</ul>}
+                </li>
     )
+}
+
+const syntaxTreeToD3 = tribosTree => {
+    if (tribosTree.children && tribosTree.children.length === 1) {
+        return syntaxTreeToD3(tribosTree.children[0]);
+    } else {
+        return {
+            name:
+                tribosTree.content.elementType === 'w' ?
+                    tribosTree.content.text :
+                    tribosTree.content.class || JSON.stringify(tribosTree.content),
+            attributes:
+                tribosTree.content.elementType === 'w' ?
+                    {
+                        gloss: tribosTree.content.gloss,
+                        class: tribosTree.content.class
+                    } :
+                    {},
+            children:
+                tribosTree.children ?
+                    tribosTree.children
+                        .filter(n => n.content.elementType !== 'pc')
+                        .map(ch => syntaxTreeToD3(ch)) :
+                    [],
+        }
+    }
+}
+
+const d3TreeDepth = (d3Tree, depth) => {
+    depth = depth || 1;
+    if (!d3Tree.children || d3Tree.children.length === 0) {
+        return depth;
+    } else {
+        return Math.max(...d3Tree.children.map(ch => d3TreeDepth(ch, depth + 1)))
+    }
+}
+
+const d3TreeLeaves = d3Tree => {
+    if (!d3Tree.children || d3Tree.children.length === 0) {
+        return 1.0;
+    } else {
+        return d3Tree.children
+            .map(ch => d3TreeLeaves(ch))
+            .reduce((a, b) => a + b);
+    }
 }
 
 const VerseDetails = ({
@@ -71,6 +121,7 @@ const VerseDetails = ({
         };
         doQuery();
     }, []);
+    const windowDimensions = useWindowDimensions();
     return <>
         <IonRow>
             <IonCol size={8}>
@@ -177,7 +228,25 @@ const VerseDetails = ({
                     </IonCol>
                 </IonRow>
                 {
-                    <ul>{renderSyntaxTree(JSON.parse(result.data.syntaxTree.document.treeSequences[0].verseTrees).data)}</ul>
+                    JSON.parse(result.data.syntaxTree.document.treeSequences[0].verseTrees).data.map(
+                        tr =>
+                            <IonRow style={{height: `${d3TreeDepth(syntaxTreeToD3(tr)) * (90 * (Math.min(1, 12.0 / d3TreeLeaves(syntaxTreeToD3(tr)))))}px`}}>
+                                <IonCol>
+                                    <Tree
+                                        data={syntaxTreeToD3(tr)}
+                                        orientation="vertical"
+                                        separation={{ nonSiblings: 0.5, siblings: 0.5 }}
+                                        nodeSize={{x:300, y:60}}
+                                        translate={{x: windowDimensions.width/2, y: 100}}
+                                        zoom={Math.min(1, 12.0/d3TreeLeaves(syntaxTreeToD3(tr)))}
+                                        rootNodeClassName="treeBranchNode"
+                                        branchNodeClassName="treeBranchNode"
+                                        leafNodeClassName="treeLeafNode"
+                                    />
+                                </IonCol>
+                            </IonRow>
+                    )
+
                 }
             </>
         }
